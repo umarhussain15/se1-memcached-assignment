92bc0b93306c66b80e458dd79963d1adfc8ca96c
Umar Hussain <umar.hussain@mailbox.tu-dresden.de>
2019-12-05 23:43:31 +0100

    client: run.sh updated to export path of dude and python before running dude commands.

--- Container2/run.sh
+++ Container2/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# set path of dude and python for this script.
+export PATH=$PATH:$HOME/local/bin
+export PYTHONPATH=$PYTHONPATH:$HOME/local/lib/python
+
 dude run
 dude sum
 Rscript graphs.R
