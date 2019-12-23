2cdec90cf4999823b8502329990a0faaab4c3a54
Umar Hussain <umar.hussain@mailbox.tu-dresden.de>
2019-12-23 22:19:33 +0100

    graphs: x-axis label graph 1 updated

--- Container2/graphs.R
+++ Container2/graphs.R
@@ -6,7 +6,7 @@ ts <- (as.numeric(Sys.time())*1000)
 trellis.device("pdf", file=paste("graph1_",ts,".pdf", sep =""), color=T, width=6.5, height=5.0)
 
 xyplot(requests ~ rate/1000, data = csvDataFrame[,c("rate","requests")],
-        xlab="Rate (k calls/sec)",
+        xlab="Rate (k.calls/sec)",
         ylab="Througput (req/sec)",
         main="Graph 1",
         type=c("p","l"))
