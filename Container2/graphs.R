9ba2b7f5756e8ca4f3d08e8b4faa895515bc4f2c
Umar Hussain <umar.hussain@mailbox.tu-dresden.de>
2019-12-23 01:58:32 +0100

    graphs: xyplot graphs added to pdf files using the data generated by dude.

--- Container2/graphs.R
+++ Container2/graphs.R
@@ -1,16 +1,22 @@
 library(lattice)
-
+Sys.time()
 read.table("output-throughput-latency/stats.csv", header=TRUE) -> csvDataFrameSource
 csvDataFrame <- csvDataFrameSource
+ts <- (as.numeric(Sys.time())*1000)
+trellis.device("pdf", file=paste("graph1_",ts,".pdf", sep =""), color=T, width=6.5, height=5.0)
 
-trellis.device("pdf", file="graph1.pdf", color=T, width=6.5, height=5.0)
+xyplot(requests ~ rate, data = csvDataFrame[,c("rate","requests")],
+        xlab="Rate",
+        ylab="Througput (req/sec)",
+        main="Graph 1",
+        type=c("p","l"))
+dev.off() -> null
 
-# ... xyplot here
+trellis.device("pdf", file=paste("graph2_",ts,".pdf", sep =""), color=T, width=6.5, height=5.0)
 
-dev.off() -> null 
-
-trellis.device("pdf", file="graph2.pdf", color=T, width=6.5, height=5.0)
-
-# ... xyplot here
-
+xyplot(latency/1000 ~ requests , data = csvDataFrame[,c("requests","latency")],
+        xlab="Througput (req/sec)",
+        ylab="Latency (s)",
+        main="Graph 2",
+        type=c("p","l"))
 dev.off() -> null 
